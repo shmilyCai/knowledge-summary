81c5094f0f613d4da0a9fe86dbe6cd9568160448
caicai <shmily.cai@gmail.com>
2018-09-28 14:35:32 +0800

    20180928

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+### 更新gh-pages下的文件
+git checkout gh-pages
+git branch
+rm -rf *~
+git checkout master _book 
+cp -ri /Users/caiyali/git/github/knowledge-summary/_book/*  /Users/caiyali/git/github/knowledge-summary
+rm -rf _book
+
+#### 提交到git上
+date=$(date +%Y%m%d)
+git add .
+git commit -am"$date"
+git pull --rebase
+git push
